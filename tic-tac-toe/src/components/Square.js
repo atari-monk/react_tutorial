fa830354cc5958bd7507739c87945a09aa7b823d
atari-monk <atari.monk1@gmail.com>
2024-06-12 21:58:45 +0200

    lift state up

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,4 +1,4 @@
-export function Square({ value, onSquareClick }) {
+export default function Square({ value, onSquareClick }) {
     return (
         <button className="square" onClick={onSquareClick}>
             {value}
